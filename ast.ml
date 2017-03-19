52f32c0445e42cec3091d0febbeb55547ec84308
Zack Silber <zs2266@columbia.edu>
2017-03-19 15:45:39 -0400

    Added t flag for tokenized string, started building token print function

--- ast.ml
+++ ast.ml
@@ -130,3 +130,88 @@ let string_of_fdecl fdecl =
 let string_of_program (vars, funcs) =
   String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
   String.concat "\n" (List.map string_of_fdecl funcs)
+
+
+
+
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Mod -> "%"
+  | Pow -> "**"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | Leq -> "<="
+  | Greater -> ">"
+  | Geq -> ">="
+  | And -> "&&"
+  | Or -> "||"
+
+let string_of_uop = function
+    Neg -> "-"
+  | Not -> "!"
+  | Deref -> "*"
+  | AddrOf -> "&"
+  | Access -> "access"
+
+let rec string_of_expr = function
+    Literal(l) -> string_of_int l
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Noexpr -> ""
+  | Null -> "NULL"  (* pointer to zero *)
+  | Inf -> "Inf"
+  | ModAssign(v, e) -> v ^ " %= " ^ string_of_expr e
+  | String(s) -> s
+  | Ch (c) -> c
+  | Subscript(s, e) -> s ^ "[" ^ string_of_expr e ^ "]"
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+  | DoWhile(s, e) -> "do { \n" ^ string_of_stmt s ^ "\n} while (" ^ string_of_expr e ^ ")\n"
+  | Break -> "break;\n"
+  | Continue -> "continue;\n"
+  | NullStmt -> ";\n"
+
+let rec string_of_typ = function
+    Int -> "INT"
+  | Char -> "CHAR"
+  | Stone -> "STONE"
+  | Mint -> "MINT"
+  | Curve -> "CURVE"
+  | Point -> "POINT"
+  | Void -> "VOID"
+  | Pointer _ as t -> "pointer " ^ string_of_typ(t)
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_tokens (vars, funcs) =
+  String.concat "" (List.map tokens_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map tokens_of_fdecl funcs)
--- cmod.ml
+++ cmod.ml
@@ -1,13 +1,14 @@
   (* Top-level of the MicroC compiler: scan & parse the input,
    check the resulting AST, generate LLVM IR, and dump the module *)
 
-type action = Ast | LLVM_IR | Compile
+type action = Ast | LLVM_IR | Compile | Tokenize
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);  (* Print the AST only *)
             ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
-            ("-c", Compile) ] (* Generate, check LLVM IR *)
+            ("-c", Compile); (* Generate, check LLVM IR *)
+            ("-t", Tokenize) ]
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in 
@@ -16,3 +17,5 @@ let _ =
     Ast -> print_string (Ast.string_of_program ast)
   | LLVM_IR -> print_string (Ast.string_of_program ast)
   | Compile -> print_string (Ast.string_of_program ast)
+  | Tokenize -> print_string (Ast.string_of_tokens ast)
+
