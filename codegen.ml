1a198094b489d73ebf1029b6d2cc2a441d8d01ef
Michael Tong <mct2159@columbia.edu>
2017-03-20 18:40:37 -0400

    Fixed extra new lines in strings

--- codegen.ml
+++ codegen.ml
@@ -82,7 +82,7 @@ let translate (globals, functions) =
     (* Construct code for an expression; return its value *)
     let rec expr builder = function
 	A.Literal i -> L.const_int i32_t i
-      | A.String s -> (*L.const_string context s*) L.build_global_stringptr (s^"\n") "fmts" builder 
+      | A.String s -> (*L.const_string context s*) L.build_global_stringptr s "fmts" builder 
       (*| A.BoolLit b -> L.const_int i1_t (if b then 1 else 0) *)
       | A.String s -> L.build_global_stringptr s s builder
       | A.Noexpr -> L.const_int i32_t 0
@@ -94,7 +94,7 @@ let translate (globals, functions) =
 	    A.Add     -> L.build_add
 	  | A.Sub     -> L.build_sub
 	  | A.Mult    -> L.build_mul
-    | A.Div     -> L.build_sdiv
+          | A.Div     -> L.build_sdiv
 	  | A.And     -> L.build_and
 	  | A.Or      -> L.build_or
 	  | A.Equal   -> L.build_icmp L.Icmp.Eq
