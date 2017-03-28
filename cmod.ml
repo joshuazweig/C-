90e17a1d60fb7b90b5cc585680996692a0cf6ca0
Joshua Zweig <jmz2135@columbia.edu>
2017-03-28 10:27:05 -0400

    Remove old pretty print features from cmod.ml

--- cmod.ml
+++ cmod.ml
@@ -1,14 +1,13 @@
 (* Top-level of the MicroC compiler: scan & parse the input,
    check the resulting AST, generate LLVM IR, and dump the module *)
 
-type action = Ast | LLVM_IR | Compile | Tokenize
+type action = Ast | LLVM_IR | Compile
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);  (* Print the AST only *)
             ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
-            ("-c", Compile);  (* Generate, check LLVM IR *)
-            ("-t", Tokenize)] 
+            ("-c", Compile) ]  (* Generate, check LLVM IR *)
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in
@@ -19,4 +18,3 @@ let _ =
   | Compile -> let m = Codegen.translate ast in
     Llvm_analysis.assert_valid_module m;
     print_string (Llvm.string_of_llmodule m)
-  | Tokenize -> print_string (Ast.string_of_tokens ast)
