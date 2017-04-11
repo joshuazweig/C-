ef0ddf956d46ae138dbbe215ac09bd03f876f4a1
Joshua Zweig <jmz2135@columbia.edu>
2017-04-11 19:32:06 -0400

    Appears to be bug in semant

--- codegen.ml
+++ codegen.ml
@@ -130,9 +130,21 @@ let translate (globals, functions) =
             let struct_c = L.undef curve_type in 
             let struct_c2 = L.build_insertvalue struct_c e1' 0 "sc" builder in 
             (L.build_insertvalue struct_c2 e2' 1 "sc2" builder, A.Curve))
-          (*last will be for oint defined with inf (not done yet) *)
+          
+          (* last would have been point type but now controlled by bit in construct3 *)
 
-     (* | A.Construct3 (e1, e2, e3) -> *)
+      | A.Construct3 (e1, e2, e3) ->
+        let (e1', t1) = expr builder e1
+        and (e2', t2) = expr builder e2
+        and (e3', t3) = expr builder e3 in 
+        (match (t1, t2, t3) with
+          (A.Curve, A.Stone, A.Stone) -> (*only construct 3?*)
+            let struct_p = L.undef point_type in
+            let struct_p2 = L.build_insertvalue struct_p e1' 0 "sp" builder in 
+            let struct_p3 = L.build_insertvalue struct_p2 e2' 1 "sp2" builder in
+            let struct_p4 = L.build_insertvalue struct_p3 e3' 2 "sp3" builder in
+            (L.build_insertvalue struct_p4 (L.const_int i1_t 0) 3 "sp4" builder, A.Point))
+      
       | A.Binop (e1, op, e2) ->
     	  let (e1', t1) = expr builder e1
     	  and (e2', t2) = expr builder e2 in (* must t1 == t2 for all binop? if so, t2 can be _ *)
