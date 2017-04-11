e57625a73b5aa7bfd9171fa72a565c310ed3d93a
Joshua Zweig <jmz2135@columbia.edu>
2017-04-11 18:40:37 -0400

    Trying to resolve struct issues

--- codegen.ml
+++ codegen.ml
@@ -42,7 +42,7 @@ let translate (globals, functions) =
     | A.Mint -> mint_type
     | A.Curve -> curve_type 
     | A.Point -> point_type 
-    | A.Pointer x -> L.pointer_type (ltype_of_typ x)  in
+    | A.Pointer x -> L.pointer_type (ltype_of_typ x) in
     (* Cant define pointer w normal form bc need type at time *)
 
   (* Declare each global variable; remember its value in a map *)
@@ -66,6 +66,9 @@ let translate (globals, functions) =
 
   let point_add_func_t = L.function_type obj_pointer [| obj_pointer ; obj_pointer |] in 
   let point_add_func = L.declare_function "point_add_func" point_add_func_t the_module in 
+
+  let stone_create_func_t = L.function_type obj_pointer [| |] in 
+  let stone_create_func = L.declare_function "BN_new" stone_create_func_t the_module in 
 
   (* Define each function (arguments and return type) so we can call it *)
   let function_decls =
@@ -114,8 +117,15 @@ let translate (globals, functions) =
       | A.Id s ->
         let binding = lookup s in
           (L.build_load (fst binding) s builder, snd binding)
-   (* | A.Construct2 (e1, e2) -> L.set struct body
-      | A.Construct3 (e1, e2, e3) -> *)
+    | A.Construct2 (e1, e2) -> 
+        let (e1', t1) = expr builder e1
+        and (e2', t2) = expr builder e2 in 
+        (match (t1, t2) with
+          (A.Stone, A.Stone) -> (L.struct_set_body mint_type [| e1' ; e2' ; 0|] 0, A.Mint)
+          | (A.Mint, A.Mint) -> (L.struct_set_body curve_type [| e1' ; e2' |] 0, A.Curve))
+          (*last will be for oint defined with inf (not done yet) *)
+
+     (* | A.Construct3 (e1, e2, e3) -> *)
       | A.Binop (e1, op, e2) ->
     	  let (e1', t1) = expr builder e1
     	  and (e2', t2) = expr builder e2 in (* must t1 == t2 for all binop? if so, t2 can be _ *)
@@ -167,9 +177,17 @@ let translate (globals, functions) =
       	     A.Neg     -> L.build_neg
             | A.Not     -> L.build_not) e' "tmp" builder, t
 
-      | A.Assign (s, e) -> let (e', t) = expr builder e in
-                       ignore (L.build_store e' (fst (lookup s)) builder); (e', t)
+      | A.Assign (s, e) -> let (e', t) = expr builder e and
+                              (* if t string, otherwise is behavior normal?*)
+                            (*snd lookup is type of thing*)
+                           ltype = (snd (lookup s)) in (match ltype with
+                           | A.Stone -> let ptr = 
+                                          L.build_call stone_create_func [| |] "stone_create_func" builder in 
+                                        L.build_store ptr (fst (lookup s)) builder; (ptr, t)
+
+                           | _ -> ignore (L.build_store e' (fst (lookup s)) builder); (e', t) )
                        
+
       | A.Call ("print", [e]) | A.Call ("printb", [e]) ->
 	       (L.build_call printf_func [| int_format_str ; fst (expr builder e) |]  "printf" builder, A.Void)
       
