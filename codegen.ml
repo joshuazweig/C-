5df29f8b952338f2ae36400c81fc88aecdc171b2
Maggie <mlm2299@columbia.edu>
2017-04-16 18:22:03 -0400

    Added some of the type stuff and prepped for linking in access

--- codegen.ml
+++ codegen.ml
@@ -34,15 +34,6 @@ let translate (globals, functions) =
 
   let rec ltype_of_typ = function
       A.Int -> i32_t
-<<<<<<< HEAD
-     | A.Char -> i8_t
-     | A.Stone -> (* Will need structs for these *) 
-     | A.Mint -> 
-     | A.Curve -> 
-     | A.Point -> 
-     | A.Pointer _ -> 
-     | A.Void -> void_t in         
-=======
     | A.Char -> i8_t (* chars are 1 byte ints *)
     | A.Void -> void_t 
     | A.Stone -> obj_pointer (* Pointer to arb prec list for C lib *)
@@ -51,7 +42,6 @@ let translate (globals, functions) =
     | A.Point -> point_type 
     | A.Pointer x -> L.pointer_type (ltype_of_typ x)  in
     (* Cant define pointer w normal form bc need type at time *)
->>>>>>> 56426d0df774ae09d0fe52eec8e58141ad6852ae
 
   (* Declare each global variable; remember its value in a map *)
   let global_vars =
@@ -63,6 +53,15 @@ let translate (globals, functions) =
   (* Declare printf(), which the print built-in function will call *)
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func = L.declare_function "printf" printf_t the_module in
+
+  let access_mint_t = L.function_type obj_pointer [| mint_type |] in
+    let access_mint = L.declare_function "access_mint" access_mint_t the_module in
+
+  let access_curve_t = L.function_type obj_pointer [| curve_type |] in
+    let access_curve = L.declare_function "access_curve" access_curve_t the_module in
+
+  let access_point_t = L.function_type obj_pointer [| point_type |] in
+    let access_point = L.declare_function "access_point" access_point_t the_module in
 
   (* Define each function (arguments and return type) so we can call it *)
   let function_decls =
@@ -105,27 +104,21 @@ let translate (globals, functions) =
 
     (* Construct code for an expression; return its value *)
     let rec expr builder = function
-<<<<<<< HEAD
 	    A.Literal i -> L.const_int i32_t i
       | A.String s -> (*L.const_string context s*) L.build_global_stringptr (s^"\n") "fmts" builder 
       (*| A.BoolLit b -> L.const_int i1_t (if b then 1 else 0) *)
       | A.String s -> L.build_global_stringptr s s builder
-=======
-	     A.Literal i -> L.const_int i32_t i
-      | A.String s -> L.build_global_stringptr s "fmts" builder 
->>>>>>> 56426d0df774ae09d0fe52eec8e58141ad6852ae
       | A.Noexpr -> L.const_int i32_t 0
       | A.Id s -> L.build_load (lookup s) s builder
-      | A.Inf ->
+(*      | A.Inf ->
       | A.Null ->
       | A.ModAssign (i, e) -> 
       | A.String s -> 
       | A.Ch c ->
       | A.Construct2 (a, b) ->              (* for both mints and curves, and points at infinity *)
       | A.Construct3 (c, a, b) ->             (* for points, on a curve *)
-      | A.Subscript (i, e) -> 
+      | A.Subscript (i, e) -> *)
       | A.Binop (e1, op, e2) ->
-<<<<<<< HEAD
       	  let e1' = expr builder e1
       	  and e2' = expr builder e2 in
       	  (match op with
@@ -133,8 +126,8 @@ let translate (globals, functions) =
         	  | A.Sub     -> L.build_sub
         	  | A.Mult    -> L.build_mul
             | A.Div     -> L.build_sdiv
-            | A.Pow     -> 
-            | A.Mod     -> 
+           (* | A.Pow     -> 
+            | A.Mod     -> *)
         	  | A.And     -> L.build_and
         	  | A.Or      -> L.build_or
         	  | A.Equal   -> L.build_icmp L.Icmp.Eq
@@ -144,33 +137,18 @@ let translate (globals, functions) =
         	  | A.Greater -> L.build_icmp L.Icmp.Sgt
         	  | A.Geq     -> L.build_icmp L.Icmp.Sge
       	  ) e1' e2' "tmp" builder
-=======
-	  let e1' = expr builder e1
-	  and e2' = expr builder e2 in
-	  (match op with
-	    A.Add     -> L.build_add
-	  | A.Sub     -> L.build_sub
-	  | A.Mult    -> L.build_mul
-          | A.Div     -> L.build_sdiv
-	  | A.And     -> L.build_and
-	  | A.Or      -> L.build_or
-	  | A.Equal   -> L.build_icmp L.Icmp.Eq
-	  | A.Neq     -> L.build_icmp L.Icmp.Ne
-	  | A.Less    -> L.build_icmp L.Icmp.Slt
-	  | A.Leq     -> L.build_icmp L.Icmp.Sle
-	  | A.Greater -> L.build_icmp L.Icmp.Sgt
-	  | A.Geq     -> L.build_icmp L.Icmp.Sge
-	  ) e1' e2' "tmp" builder
->>>>>>> 56426d0df774ae09d0fe52eec8e58141ad6852ae
       | A.Unop(op, e) ->
       	  let e' = expr builder e in
       	  (match op with
-      	    A.Neg     -> L.build_neg
-           | A.Not     -> L.build_not
-           | A.Deref   ->
-           | A.AddrOf  -> 
-           | A.Access  ->
-          ) e' "tmp" builder
+      	    A.Neg     -> L.build_neg e' "tmp" builder
+           | A.Not     -> L.build_not e' "tmp" builder
+           | A.Deref   -> L.build_load e' "tmp" builder  (* load object pointed to *)
+           (* | A.AddrOf  -> L.build_store e' (lookup ??) builder  (* create pointer to address of object -- want what is returned by L.build_alloca -- could just move stuff everytime? seems inefficient *)
+           | A.Access  -> 
+              if t = A.Mint then L.build_call access_mint [| e' |] "access_mint" builder
+              else if t = A.Curve then L.build_call access_curve [| e' |] "access_curve" builder
+              else if t = A.Point then L.build_call access_point [| e' |] "access_point" builder
+          ) *)(* e' "tmp" builder *)
       | A.Assign (s, e) -> let e' = expr builder e in
 	                   ignore (L.build_store e' (lookup s) builder); e'
       | A.Call ("print", [e]) | A.Call ("printb", [e]) ->
@@ -201,17 +179,12 @@ let translate (globals, functions) =
     let rec stmt builder = function
 	      A.Block sl -> List.fold_left stmt builder sl
       | A.Expr e -> ignore (expr builder e); builder
-      | A.Break ->
+     (* | A.Break ->
       | A.Continue -> 
-      | A.NullStmt ->
+      | A.NullStmt ->*)
       | A.Return e -> ignore (match fdecl.A.typ with
-<<<<<<< HEAD
 	         A.Void -> L.build_ret_void builder
           | _ -> L.build_ret (expr builder e) builder); builder
-=======
-	                 A.Void -> L.build_ret_void builder
-	    | _ -> L.build_ret (expr builder e) builder); builder
->>>>>>> 56426d0df774ae09d0fe52eec8e58141ad6852ae
       | A.If (predicate, then_stmt, else_stmt) ->
            let bool_val = expr builder predicate in
         	 let merge_bb = L.append_block context "merge" the_function in
@@ -242,8 +215,8 @@ let translate (globals, functions) =
       	  ignore (L.build_cond_br bool_val body_bb merge_bb pred_builder);
       	  L.builder_at_end context merge_bb
 
-      | A.DoWhile (body, predicate) ->   (* Need to UPDATE (while to do while) !!! *)
-
+    (*  | A.DoWhile (body, predicate) ->   (* Need to UPDATE (while to do while) !!! *)
+*)
 
       | A.For (e1, e2, e3, body) -> stmt builder
 	       ( A.Block [A.Expr e1 ; A.While (e2, A.Block [body ; A.Expr e3]) ] )
