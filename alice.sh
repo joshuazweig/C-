f9e13ce82cd7461be83ea50910624b53e8116743
Joshua Zweig <jmz2135@columbia.edu>
2017-04-26 15:29:45 -0400

    alice and bob not doing it

--- alice.sh
+++ alice.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#Mdb lookup script
 
 mkfifo mypipe-$$
-cat mypipe-$$ | nc -l  $1 | ./cmod.sh alice.cm | bc > tee  mypipe-$$
+cat mypipe-$$ | nc -l  $1 | ./cmod.sh alice.cm | bc > mypipe-$$
 
 rm mypipe-$$
--- bob.sh
+++ bob.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#Mdb lookup script
 
 mkfifo mypipe-$$
 cat mypipe-$$ | nc localhost $1 | ./cmod.sh bob.cm | bc > mypipe-$$
