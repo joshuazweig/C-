487e360c2f9ea56f27d61d501b5b4205396571a7
Maggie <mlm2299@columbia.edu>
2017-05-06 12:51:59 -0400

    Cut tabbing in help message

--- cmc.sh
+++ cmc.sh
@@ -10,18 +10,18 @@ TEST="$2"
 
 usage() { echo "Usage: $0 [-h help] [-t token] [-a ast] [-l llvm] [-c ll-file] [-s s-file] [-e exe-file] <file-name>.cm" 1>&2; exit 1; }
 
-help() { echo "\n \t Welcome to the C% compiler CMC! 
-				\n \t \t USAGE: $0 [-h help] [-t token] [-a ast] [-l llvm] [-c ll-file] [-s s-file] [-e exe-file] <file-name>.cm\n 
-				\n \t \t OPTIONS:
-				-h 	help  		This option prints this message!\n
-				-t	token		This option prints the tokenized program to stdout.\n
-				-a 	ast 		This option prints the abstract syntax tree of the program to stdout.\n
-				-l 	llvm		Compiles <file-name>.cm to llvm and prints the result to stdout.\n
-				-c 	ll-file		Compiles <file-name>.cm to llvm and puts the result in <file-name>.ll. This is the default option.\n
-				-s 	assembly	Compiles <file-name>.cm to llvm, translates to assembly, and puts the result in <file-name>.s 
-								(leaves <file-name>.ll in directory as well)\n
-				-e 	executable	Creates the executable version of <file-name>.cm, simply called <file-name> to be run ./<file-name> 
-								(leaves behind the corresponding .ll and .s files as well)\n" 1>&2; exit 1; }
+help() { echo "\n Welcome to the C% compiler CMC! 
+	\n USAGE: $0 [-h help] [-t token] [-a ast] [-l llvm] [-c ll-file] [-s s-file] [-e exe-file] <file-name>.cm\n 
+	\n OPTIONS:
+	-h 	help  		This option prints this message!\n
+	-t	token		This option prints the tokenized program to stdout.\n
+	-a 	ast 		This option prints the abstract syntax tree of the program to stdout.\n
+	-l 	llvm		Compiles <file-name>.cm to llvm and prints the result to stdout.\n
+	-c 	ll-file		Compiles <file-name>.cm to llvm and puts the result in <file-name>.ll. This is the default option.\n
+	-s 	assembly	Compiles <file-name>.cm to llvm, translates to assembly, and puts the result in <file-name>.s 
+					(leaves <file-name>.ll in directory as well)\n
+	-e 	executable	Creates the executable version of <file-name>.cm, simply called <file-name> to be run ./<file-name> 
+					(leaves behind the corresponding .ll and .s files as well)\n" 1>&2; exit 1; }
 
 if getopts "h:t:a:l:c:s:e:" c; then
 	basename=`echo "$TEST" | sed 's/.*\\///
